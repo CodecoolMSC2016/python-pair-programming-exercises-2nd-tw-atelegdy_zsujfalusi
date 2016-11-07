3d4b9af586de040f75242d0b2746b741315356ae
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2016-11-07 15:38:35 +0100

    driver: zsujfalusi (extras done too)

--- years/years_module.py
+++ years/years_module.py
@@ -2,10 +2,20 @@ import datetime
 
 
 def years(age):
-    return
+    today = datetime.date.today()
+    hundredyears = int(today.year) + (100 - age)
+    return hundredyears
 
 
 def main():
+    name = str(input("Please tell me your name: "))
+    age = int(input("Please tell me your age: "))
+    multiplier = int(input("How many times would you like to see the answer? "))
+
+    hundredyears = years(age)
+    for i in range(0, multiplier):
+        print("Kedves {0}, Te {1}-ban/ben leszel 100 éves!".format(name, hundredyears))
+
     return
 
 

9535a057158d388c23da1fdba23180632473bfc3
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2016-11-07 14:48:34 +0100

    driver: zsujfalusi

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,24 @@
 def palindrome(str):
-    return
+    transformed = ""
+
+    for char in str:
+        if char != " ":
+            transformed += char.lower()
+
+    checker = True
+    j = len(transformed) - 1
+    for i in range(0, len(transformed)):
+        if transformed[i] != transformed[j]:
+            checker = False
+        j -= 1
+
+    return checker
 
 
 def main():
+    bemenet = str(input("Please provide the text you want to check: "))
+
+    check = palindrome(transformed)
     return
 
 

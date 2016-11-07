b0d234d7f1466fd338210fcf656f8a03c8c9d08c
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2016-11-07 13:54:02 +0100

    driver: zsujfalusi

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,21 @@
 def fizzbuzz(number):
-    return
+    if number % 3 == 0 and number % 5 == 0:
+        number = "FizzBuzz"
+    elif number % 3 == 0:
+        number = "Fizz"
+    elif number % 5 == 0:
+        number = "Buzz"
+
+    print(number)
+    return number
 
 
 def main():
+    numbers = []
+
+    for i in range(0, 100):
+        numbers.append(i + 1)
+        fizzbuzz(numbers[i])
     return
 
 if __name__ == '__main__':

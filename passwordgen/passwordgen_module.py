4ad5707c9fe2db3cb72912f1f72a7689207f5c29
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2016-11-07 15:17:52 +0100

    driver: átelegdy

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,35 @@
+import random
+
+
 def passwordgen():
-    return
+    lowerLetters = ("abcdefghijlkmnopqrstuvwxyz")
+    upperLetters = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    numbers = ("0123456789")
+    symbols = ("!@#$%^&*()?")
+    words = ['pass1234', 'jelszo12', 'password', 'kutya678', 'cicacica']
+    password = ""
+    pwlength = random.randint(8, 16)
+
+    while len(password) <= pwlength:
+        pwtype = random.randint(1, 4)
+        if pwtype == 1:
+            pwchar = lowerLetters[random.randint(0, len(lowerLetters) - 1)]
+            password += pwchar
+        elif pwtype == 2:
+            pwchar = upperLetters[random.randint(0, len(upperLetters) - 1)]
+            password += pwchar
+        elif pwtype == 3:
+            pwchar = numbers[random.randint(0, len(numbers) - 1)]
+            password += pwchar
+        elif pwtype == 4:
+            pwchar = symbols[random.randint(0, len(symbols) - 1)]
+            password += pwchar
+
+    return password
 
 
 def main():
+    passwordgen()
     return
 
 

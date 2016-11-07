f01905fc18e41f91a20cddc73916f1343ff1460a
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2016-11-07 16:14:56 +0100

    driver: shared

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -10,20 +10,34 @@ def passwordgen():
     password = ""
     pwlength = random.randint(8, 16)
 
-    while len(password) <= pwlength:
+    lower = False
+    upper = False
+    number = False
+    symbol = False
+
+    while len(password) <= pwlength or (lower == False or upper == False or number == False or symbol == False):
+
         pwtype = random.randint(1, 4)
         if pwtype == 1:
             pwchar = lowerLetters[random.randint(0, len(lowerLetters) - 1)]
             password += pwchar
+            lower = True
+
         elif pwtype == 2:
             pwchar = upperLetters[random.randint(0, len(upperLetters) - 1)]
             password += pwchar
+            upper = True
+
         elif pwtype == 3:
             pwchar = numbers[random.randint(0, len(numbers) - 1)]
             password += pwchar
+            number = True
+
         elif pwtype == 4:
             pwchar = symbols[random.randint(0, len(symbols) - 1)]
             password += pwchar
+            symbol = True
+    print(password)
 
     return password
 
